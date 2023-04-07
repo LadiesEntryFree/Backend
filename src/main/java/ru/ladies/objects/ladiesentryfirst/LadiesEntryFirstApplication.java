0427fe4094ee500c1cdd059904e206fe22fd0341
i.gorin <i.gorin@g.nsu.ru>
2023-04-08 00:18:24 +0700

    project init commit

---
+++ src/main/java/ru/ladies/objects/ladiesentryfirst/LadiesEntryFirstApplication.java
@@ -0,0 +1,13 @@
+package ru.ladies.objects.ladiesentryfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadiesEntryFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LadiesEntryFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ladies/objects/ladiesentryfirst/LadiesEntryFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ladies.objects.ladiesentryfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadiesEntryFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
