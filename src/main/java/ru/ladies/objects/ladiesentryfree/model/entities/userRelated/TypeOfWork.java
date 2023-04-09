4411f31751f2c3c5c9c71cd83ca9dea0a48ceba0
i.gorin <i.gorin@g.nsu.ru>
2023-04-09 13:08:43 +0700

    исправил enum по типам работ

--- src/main/java/ru/ladies/objects/ladiesentryfree/model/entities/userRelated/TypeOfWork.java
+++ src/main/java/ru/ladies/objects/ladiesentryfree/model/entities/userRelated/TypeOfWork.java
@@ -1,16 +1,10 @@
 package ru.ladies.objects.ladiesentryfree.model.entities.userRelated;
 
 public enum TypeOfWork {
-    ANALYZE("Анализ работ"),
-    BUILD("Строительство"),
-    FIX("Ремонт"),
-    ESTIMATION("Оценка работ"),
-    DESTROY("Демонтаж"),
-    INSPECTION("Инспекция");
-
-    private String name;
-
-    TypeOfWork(String name) {
-        this.name = name;
-    }
+    ANALYZE,
+    BUILD,
+    FIX,
+    ESTIMATION,
+    DESTROY,
+    INSPECTION
 }
